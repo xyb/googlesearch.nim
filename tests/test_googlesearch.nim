0c3a03fd9fbd59e20343bc95fd45a4215f8c27af
Xie Yanbo <xieyanbo@gmail.com>
2019-11-22 23:20:04 +0800

    test: more pages

--- tests/test_googlesearch.nim
+++ tests/test_googlesearch.nim
@@ -12,3 +12,9 @@ import googlesearch
 test "search nim lang":
   for result in search("nim lang", 1):
     check result.url == "https://nim-lang.org/"
+
+test "search more pages":
+  var total = 0
+  for result in search("nim lang", 20):
+    total += 1
+  check total == 20
